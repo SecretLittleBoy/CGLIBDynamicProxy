875f030105460d8f15d56fc98ed044a21566f1af
LYH <3210101621@zju.edu.cn>
2024-04-22 19:25:35 +0800

    update

---
+++ src/main/java/com/github/SecretLittleBoy/App.java
@@ -0,0 +1,14 @@
+package com.github.SecretLittleBoy;
+
+import com.github.SecretLittleBoy.sms.AliSmsService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        AliSmsService aliSmsService = (AliSmsService) CglibProxyFactory.getProxy(AliSmsService.class);
+        aliSmsService.send("java");
+    }
+}
